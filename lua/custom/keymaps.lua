3d3ab015330d400d965e97c6808ffd79f3286820
Zach Drone <zvdrone@gmail.com>
2025-01-05 02:06:11 -0500

    add fix list nav

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -30,3 +30,9 @@ end)
 
 -- tmux-sessionizer
 vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
+
+-- fix/location list nav
+vim.keymap.set('n', '<C-j>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-k>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>k', '<cmd>lprev<CR>zz')
