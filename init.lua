fee5bd1817c1c1defd6ac09623131350b9e2d6cd
Zach Drone <zvdrone@gmail.com>
2025-05-24 14:12:22 -0400

    modularize it all

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
-require 'custom.options'
-require 'custom.lazy'
-require 'custom.keymaps'
-require 'custom.autocommands'
+require 'custom.config.options'
+require 'custom.config.lazy'
+require 'custom.config.keymaps'
+require 'custom.config.autocmds'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
